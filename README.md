cc553e43b61308a5d16980e243fb201c35a6d617
Dharmesh Vishwakarma <dharmeshvishwakarma2000@gmail.com>
2021-10-03 22:14:44 +0530

    ver1.0.0

--- README.md
+++ README.md
@@ -1,3 +1,57 @@
-"# GithubInfo" 
-"# GithubUserInfo" 
 # GithubUserInfo
+
+This package is only for Github Public User's Account.
+
+## Installation of Package
+```bash
+$ pip install GithubUserInfo
+```
+
+## Simple Demo
+
+```python
+# import the package
+from GithubUserInfo import githubUserAccount as gt
+
+# using username get details of github account
+gt.get("username")
+
+# using username set the username & call other functions
+gt.set("username")
+
+# returns user's name
+gt.name()
+
+# returns user's username
+gt.username()
+
+# returns user's bio
+gt.bio()
+
+# returns user's total followers
+gt.followers()
+
+# returns user's total followings
+gt.following()
+
+# returns user's total stars
+gt.star()
+
+# returns user's location
+gt.location()
+
+# returns user's website
+gt.website()
+
+# returns user's twitter
+gt.twitter()
+
+# returns user's organization
+gt.organization()
+
+# returns user's total projects
+gt.count_projects()
+
+# returns user's repositories
+gt.count_repositories()
+```
--- setup.py
+++ setup.py
@@ -1,74 +1,21 @@
 from distutils.core import setup
-import textwrap
 setup(
   name = 'GithubUserInfo',         # How you named your package folder (MyLib)
   packages = ['GithubUserInfo'],   # Chose the same as "name"
-  version = '1.1',      # Start with a small number and increase it with every change you make
+  version = '1.0.0',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = "Find the user's github account information.",   # Give a short description about your library
   author = 'Dharmesh Vishwakarma',                   # Type in your name
   author_email = 'dharmeshvishwakarma2000@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/imvdharmesh/GithubUserInfo',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/imvdharmesh/GithubUserInfo/archive/refs/tags/pypi-1.1.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/imvdharmesh/GithubUserInfo/archive/refs/tags/pypi-1.0.0.tar.gz',    # I explain this later on
   keywords = ['github', 'githubuserinfo', 'githubuseraccount'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'requests',
           'beautifulsoup4',
       ],
-  long_description =textwrap.dedent(
-    """\
-    (Tutorial)
-    ===========================
-
-    This package is only for Github Public User's Account.
-
-      # import the package
-      from GithubUserInfo import githubUserAccount as gt
-
-      # using username get details of github account
-      gt.get("username")
-
-      # using username set the username & call other functions
-      gt.set("username")
-
-      # returns user's name
-      gt.name()
-
-      # returns user's username
-      gt.username()
-
-      # returns user's bio
-      gt.bio()
-
-      # returns user's total followers
-      gt.followers()
-
-      # returns user's total followings
-      gt.following()
-
-      # returns user's total stars
-      gt.star()
-
-      # returns user's location
-      gt.location()
-
-      # returns user's website
-      gt.website()
-
-      # returns user's twitter
-      gt.twitter()
-
-      # returns user's organization
-      gt.organization()
-
-      # returns user's total projects
-      gt.count_projects()
-
-      # returns user's repositories
-      gt.count_repositories()
-
-    /"""
-    ),
+  long_description = 'file:README.md',
+  long_description_content_type = 'text/markdown',
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
